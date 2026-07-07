ddf2c2e10a72b438f94d4354aa00dcbad8fadd31
IzaakCod <zaaknascimentosantos@gmail.com>
2026-07-07 17:45:05 -0300

    Novas formas de cris uma classe, de jeitos diferentes!!!

--- POO-TypeScript/293-Classes-em-TS-Moldes-para-objetos.js
+++ POO-TypeScript/293-Classes-em-TS-Moldes-para-objetos.js
@@ -1,11 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Empresa = void 0;
+exports.Colaborador = exports.Empresa = void 0;
 class Empresa {
-    constructor(nome) {
+    constructor(nome, cnpj) {
+        this._colaboradores = [];
         this.nome = nome;
+        this.cnpj = cnpj;
+    }
+    //Isso aqui é um metódo, o primeiro colaborador é o valor
+    // e o segundo é o tipo dele!!!
+    adicionaColaborador(colaborador) {
+        this._colaboradores.push(colaborador);
+    }
+    mostrarColaboradores() {
+        for (const colaborador of this._colaboradores) {
+            console.log(colaborador);
+        }
     }
 }
 exports.Empresa = Empresa;
-const empresa1 = new Empresa("Facebook");
+// Modo simplificado 
+class Colaborador {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+exports.Colaborador = Colaborador;
+const empresa1 = new Empresa("Udemy", "11.111.111/0001-11");
+const colaborador1 = new Colaborador("Izaak", "Santos");
+const colaborador2 = new Colaborador("Davi", "Souza");
+const colaborador3 = new Colaborador("Flávia", "Lima");
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
 console.log(empresa1);
+empresa1.mostrarColaboradores();
--- POO-TypeScript/293-Classes-em-TS-Moldes-para-objetos.ts
+++ POO-TypeScript/293-Classes-em-TS-Moldes-para-objetos.ts
@@ -1,13 +1,43 @@
 export class Empresa {
     // readonly serve para dizer que o atributo não
-    // pode ser alterado, só podendo ser lido fora da classe!!!
-    public readonly nome: string
+    // pode ser alterado, só podendo ser lido!!!
 
-    constructor(nome: string) {
-        this.nome = nome 
+    public readonly nome: string // public não necessário
+    private readonly _colaboradores: Colaborador[] = []
+    protected readonly cnpj: string
+
+    constructor(nome: string, cnpj: string) {
+        this.nome = nome
+        this.cnpj = cnpj
+    }
+    //Isso aqui é um metódo, o primeiro colaborador é o valor
+    // e o segundo é o tipo dele!!!
+    adicionaColaborador(colaborador: Colaborador): void {
+        this._colaboradores.push(colaborador)
+    }
+
+    mostrarColaboradores(): void {
+        for (const colaborador of this._colaboradores) {
+            console.log(colaborador)
+        }
+    }
+
+}
+// Modo simplificado 
+export class Colaborador {
+    constructor(
+        public readonly nome: string,
+        public readonly sobrenome: string) {
+
     }
 }
 
-
-const empresa1 = new Empresa("Facebook")
+const empresa1 = new Empresa("Udemy", "11.111.111/0001-11")
+const colaborador1 = new Colaborador("Izaak", "Santos")
+const colaborador2 = new Colaborador("Davi", "Souza")
+const colaborador3 = new Colaborador("Flávia", "Lima")
+empresa1.adicionaColaborador(colaborador1)
+empresa1.adicionaColaborador(colaborador2)
+empresa1.adicionaColaborador(colaborador3)
 console.log(empresa1)
+empresa1.mostrarColaboradores()
